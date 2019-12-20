de391f3ae450e4ab83c1427ea980628a2d5efa39
Nikhil Anand <mail@nikhil.io>
2019-12-20 14:38:26 -0600

    "Enhancements", mostly
    Prettier
    CSS rules
    Use $ and $$ and be lazy
    Search can be dismissed with Esc

--- sorry.nikhil.io/excuses.js
+++ sorry.nikhil.io/excuses.js
@@ -1,61 +1,64 @@
-class TextScramble {
+(() => {
+
+  class TextScramble {
     constructor(el) {
-        this.el = el;
-        this.chars = "!<>-_\\/[]{}—=+*^?#_______";
-        this.update = this.update.bind(this);
+      this.el = el;
+      this.chars = "!<>-_\\/[]{}—=+*^?#_______";
+      this.update = this.update.bind(this);
     }
     setText(newText) {
-        const oldText = this.el.innerText;
-        const length = Math.max(oldText.length, newText.length);
-        const promise = new Promise(resolve => (this.resolve = resolve));
-        this.queue = [];
-        for (let i = 0; i < length; i++) {
-            const _from = oldText[i] || "";
-            const to = newText[i] || "";
-            const start = Math.floor(Math.random() * 15);
-            const end = start + Math.floor(Math.random() * 30);
-            this.queue.push({ _from, to, start, end });
-        }
-        cancelAnimationFrame(this.frameRequest);
-        this.frame = 0;
-        this.update();
-        return promise;
+      const oldText = this.el.innerText;
+      const length = Math.max(oldText.length, newText.length);
+      const promise = new Promise(resolve => (this.resolve = resolve));
+      this.queue = [];
+      for (let i = 0; i < length; i++) {
+        const _from = oldText[i] || "";
+        const to = newText[i] || "";
+        const start = Math.floor(Math.random() * 15);
+        const end = start + Math.floor(Math.random() * 30);
+        this.queue.push({ _from, to, start, end });
+      }
+      cancelAnimationFrame(this.frameRequest);
+      this.frame = 0;
+      this.update();
+      return promise;
     }
     update() {
-        let output = "";
-        let complete = 0;
-        for (let i = 0, n = this.queue.length; i < n; i++) {
-            let { _from, to, start, end, char } = this.queue[i];
-            if (this.frame >= end) {
-                complete++;
-                output += to;
-            } else if (this.frame >= start) {
-                if (!char || Math.random() < 0.28) {
-                    char = this.randomChar();
-                    this.queue[i].char = char;
-                }
-                output += `<span class="filler"> ${char}</span>`;
-            } else {
-                output += _from;
-            }
-        }
-        this.el.innerHTML = output;
-        if (complete === this.queue.length) {
-            this.resolve();
+      let output = "";
+      let complete = 0;
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { _from, to, start, end, char } = this.queue[i];
+        if (this.frame >= end) {
+          complete++;
+          output += to;
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar();
+            this.queue[i].char = char;
+          }
+          output += `<span class="filler"> ${char}</span>`;
         } else {
-            this.frameRequest = requestAnimationFrame(this.update);
-            this.frame++;
+          output += _from;
         }
+      }
+      this.el.innerHTML = output;
+      if (complete === this.queue.length) {
+        this.resolve();
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update);
+        this.frame++;
+      }
     }
     randomChar() {
-        return this.chars[Math.floor(Math.random() * this.chars.length)];
+      return this.chars[Math.floor(Math.random() * this.chars.length)];
     }
-}
+  }
 
-const scrambler = new TextScramble(document.querySelector("#excuse-text"));
-const API_ENDPOINT = "/excuses.json";
-let EXCUSES = {};
-const suffixEmojis = [
+  const $ = document.querySelector.bind(document);
+  const $$ = document.querySelectorAll.bind(document);
+  const scrambler = new TextScramble($(".excuse-text"));
+  const API_ENDPOINT = "/excuses.json";
+  const suffixEmojis = [
     "🤷‍♂️",
     "🤷‍♀️",
     "🤦‍♀️",
@@ -68,140 +71,150 @@ const suffixEmojis = [
     "🙄",
     "👍",
     "💁‍♀️",
-    "💁‍♂️"
-];
+    "💁‍♂️",
+  ];
+  let EXCUSES = {};
 
-const randomSuffixEmoji = () =>
+  const randomSuffixEmoji = () =>
     suffixEmojis[(suffixEmojis.length * Math.random()) << 0];
 
-const randomExcuse = excuses => {
+  const randomExcuse = excuses => {
     const keys = Object.keys(excuses);
     const randomKey = keys[(keys.length * Math.random()) << 0];
 
     return [randomKey, excuses[randomKey]];
-};
+  };
 
-const showErrorState = () => {
-    document.querySelector("#loading").style.display = "none";
-    document.querySelector("#oh-crap").style.display = "table-cell";
+  const showErrorState = () => {
+    $(".loading").style.display = "none";
+    $(".oh-crap").style.display = "table-cell";
 
     return false;
-};
+  };
 
-const showExcuse = forceRefresh => {
+  const showExcuse = forceRefresh => {
     let excuse;
     let hash;
     const suffixEmoji = randomSuffixEmoji();
 
     if (window.location.hash && forceRefresh !== true) {
-        const excuseHash = window.location.hash.split("#")[1];
+      const excuseHash = window.location.hash.split("#")[1];
 
-        if (Object.keys(EXCUSES).indexOf(excuseHash) === -1) {
-            [hash, excuse] = randomExcuse(EXCUSES);
-        } else {
-            excuse = EXCUSES[excuseHash];
-            hash = excuseHash;
-        }
-    } else {
+      if (Object.keys(EXCUSES).indexOf(excuseHash) === -1) {
         [hash, excuse] = randomExcuse(EXCUSES);
+      } else {
+        excuse = EXCUSES[excuseHash];
+        hash = excuseHash;
+      }
+    } else {
+      [hash, excuse] = randomExcuse(EXCUSES);
     }
 
     history.pushState(null, null, "#" + hash);
 
-    document.querySelector("#loading").style.display = "none";
+    $(".loading").style.display = "none";
+    $(".reaction").innerText = "";
     document.title = "I'm Sorry; " + excuse;
-    document.querySelector("#reaction").innerText = '';
 
     scrambler.setText(excuse).then(() => {
-        document.querySelector("#reaction").innerText = suffixEmoji;
+      $(".reaction").innerText = suffixEmoji;
     });
 
     return true;
-};
+  };
 
-const getExcuses = () =>
+  const getExcuses = () =>
     fetch(API_ENDPOINT, { cache: "force-cache" })
-        .then(response => {
-            if (!response.ok) {
-                showErrorState();
-            } else {
-                return response.json();
-            }
-        })
-        .then(excuses => {
-            EXCUSES = excuses;
-            return EXCUSES;
-        })
-        .catch(e => {
-            showErrorState();
-        });
+      .then(response => {
+        if (!response.ok) {
+          showErrorState();
+        } else {
+          return response.json();
+        }
+      })
+      .then(excuses => {
+        EXCUSES = excuses;
+        return EXCUSES;
+      })
+      .catch(e => {
+        showErrorState();
+      });
 
-const createHTMLLink = (text, url) => {
+  const createHTMLLink = (text, url) => {
     let a = document.createElement("a");
     a.appendChild(document.createTextNode(text));
     a.setAttribute("href", `/#${url}`);
     a.setAttribute("class", "excuse-link");
 
     return a;
-};
+  };
 
-const createListItem = child => {
+  const createListItem = child => {
     let li = document.createElement("li");
     li.appendChild(child);
 
     return li;
-};
+  };
 
-const toggleList = () => {
-    const vis = document.querySelector("#list-wrapper").style.visibility;
+  const toggleList = () => {
+    const vis = $(".list-wrapper").style.visibility;
 
     if (vis === "visible") {
-        document.querySelector("#list-wrapper").style.visibility = "hidden";
+      $(".list-wrapper").style.visibility = "hidden";
     } else {
-        document.querySelector("#list-wrapper").style.visibility = "visible";
+      $(".list-wrapper").style.visibility = "visible";
     }
-};
+  };
 
-const searchFor = (term, links) => {
+  const searchFor = (term, links) => {
     const _term = term.toLowerCase();
 
     for (i = 0; i < links.length; i++) {
-        if (!links[i].innerHTML.toLowerCase().includes(_term)) {
-            links[i].parentElement.style.display = "none";
-        } else {
-            links[i].parentElement.style.display = "list-item";
-        }
+      if (!links[i].innerHTML.toLowerCase().includes(_term)) {
+        links[i].parentElement.style.display = "none";
+      } else {
+        links[i].parentElement.style.display = "list-item";
+      }
     }
-};
+  };
 
-window.onload = () => {
+  window.onload = () => {
     getExcuses().then(excuses => {
-        showExcuse();
+      showExcuse();
 
-        const list = document.querySelector("#list");
-        Object.keys(excuses).map(hash =>
-            list.appendChild(
-                createListItem(createHTMLLink(excuses[hash], hash))
-            )
-        );
+      const list = $(".list");
+      Object.keys(excuses).map(hash =>
+        list.appendChild(createListItem(createHTMLLink(excuses[hash], hash))),
+      );
 
-        const excuseLinks = document.querySelectorAll(".excuse-link");
-        for (let i = excuseLinks.length - 1; i >= 0; i--) {
-            excuseLinks[i].addEventListener("click", () => toggleList());
-        }
+      const excuseLinks = $$(".excuse-link");
+      for (let i = excuseLinks.length - 1; i >= 0; i--) {
+        excuseLinks[i].addEventListener("click", () => toggleList());
+      }
 
-        document
-            .querySelector("#search")
-            .addEventListener("keyup", e =>
-                searchFor(e.target.value, excuseLinks)
-            );
+      $(".search").addEventListener("keyup", e =>
+        searchFor(e.target.value, excuseLinks),
+      );
     });
-};
+  };
 
-window.addEventListener("popstate", e => showExcuse());
-document
-    .querySelector("#wrapper")
-    .addEventListener("click", e => showExcuse(true));
-document
-    .querySelector("#show-list")
-    .addEventListener("click", e => toggleList());
+  document.onkeydown = e => {
+    e = e || window.event;
+    let isEscape = false;
+
+    if ("key" in e) {
+      isEscape = e.key === "Escape" || e.key === "Esc";
+    } else {
+      isEscape = e.keyCode === 27;
+    }
+
+    if (isEscape && $(".list-wrapper").style.visibility == "visible") {
+      $(".list-wrapper").style.visibility = "hidden";
+    }
+  };
+
+  window.addEventListener("popstate", e => showExcuse());
+  $(".wrapper").addEventListener("click", e => showExcuse(true));
+  $(".show-list").addEventListener("click", e => toggleList());
+
+})();
